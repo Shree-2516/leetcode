b869abfccf1fb2dbc4f7155fd398dc72054ea0d2
Shreeyash Paraj <shreeyash2573@gmail.com>
2026-07-24 13:18:23 +0530

    Time: 0 ms (100%), Space: 20.3 MB (63.68%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums, target):
+        hashmap = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in hashmap:
+                return [hashmap[complement], i]
+
+            hashmap[num] = i
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
