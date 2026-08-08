85575373f6bddaa18b150ebe561822c0351a53e8
Shreeyash Paraj <shreeyash2573@gmail.com>
2026-08-08 10:21:46 +0530

    Time: 410 ms (86.96%), Space: 46.5 MB (86.96%) - LeetHub - Time: O(n + m), Space: O(m)

---
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
@@ -0,0 +1,43 @@
+class Solution:
+    def validSequence(self, word1: str, word2: str) -> list[int]:
+        n = len(word1)
+        m = len(word2)
+
+        last = [-1] * m
+
+        i = n - 1
+        j = m - 1
+
+        while i >= 0 and j >= 0:
+            if word1[i] == word2[j]:
+                last[j] = i
+                j -= 1
+            i -= 1
+
+        ans = []
+        j = 0
+        changed = False
+
+        for i in range(n):
+            if j == m:
+                break
+
+            if word1[i] == word2[j]:
+                ans.append(i)
+                j += 1
+
+            elif not changed and (
+                j == m - 1 or i < last[j + 1]
+            ):
+                ans.append(i)
+                changed = True
+                j += 1
+
+        if j == m:
+            return ans
+
+        return []
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
